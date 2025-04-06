7a2204b10406924e43cd61b43f73c9abe4c62232
TotalBattleBots <noreply@github.com>
2025-04-05 20:22:58 -0400

    fix Linux-TestScreenshot0.py

--- Linux-TestScreenshot1.py
+++ Linux-TestScreenshot1.py
@@ -7,15 +7,8 @@ import mss
 with mss.mss() as sct:
     # Print all available monitors
     print(sct.monitors)
-
+    
     # Try grabbing a specific monitor (e.g., the first one)
     screenshot = sct.grab(sct.monitors[1])  # Adjust index if needed
     screenshot.save("screenshot1.png")
     
-    # Try grabbing a specific monitor (e.g., the first one)
-    screenshot = sct.grab(sct.monitors[2])  # Adjust index if needed
-    screenshot.save("screenshot2.png")
-    
-    # Try grabbing a specific monitor (e.g., the first one)
-    screenshot = sct.grab(sct.monitors[0])  # Adjust index if needed
-    screenshot.save("screenshot0.png")
